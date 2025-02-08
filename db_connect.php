97dd8e523d3f877590ccdefc0a23e29f9fa98bd0
Rasika-Udara <dgrasikaudaralk@gmail.com>
2025-02-09 01:18:25 +0530

    db updated

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "sql213.infinityfree.com"; // Use "localhost" instead of "127.0.0.1"
-$username = "if0_38272929";        // MySQL default username
-$password = "Hibabycool762";            // Default XAMPP password is empty
-$database = "if0_38272929_spd_hub";     // Database name
+$servername = "localhost"; // Use "localhost" instead of "127.0.0.1"
+$username = "root";        // MySQL default username
+$password = "";            // Default XAMPP password is empty
+$database = "spd_hub";     // Database name
 
 // Create a connection to MySQL
 $conn = new mysqli($servername, $username, $password, $database);
